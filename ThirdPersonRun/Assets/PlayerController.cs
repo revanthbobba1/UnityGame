44cf7f7a37bda10473aad063d9d2190dfedd8d0e
Keshav Sridhar <keshavsridhar9307@gmail.com>
2020-05-08 21:16:47 -0400

    First Controllers

---
+++ ThirdPersonRun/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 2;
+    public float runSpeed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    	Vector2 inputDir = input.normalized;
+
+        if(inputDir != Vector2.zero) {
+            transform.eulerAngles = Vector3.up * Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+        }
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        float speed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+
+        transform.Translate(-1 * transform.forward * speed * Time.deltaTime, Space.World);
+    }
+}
