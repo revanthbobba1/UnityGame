d55fa184312928f2ca54aa893ff0229c046ec05f
Shaan Gondalia <shaan.gondalia@duke.edu>
2020-06-02 20:51:53 -0400

    Initial Commit

---
+++ ElementProject/Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+	public float walkSpeed = 2;
+	public float runSpeed = 6;
+    public float gravity = 9.8f;
+    public float jumpHeight = 1;
+    [Range(0,1)]
+    public float airControlPercent;
+
+    public bool grounded;
+
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
+
+	public float speedSmoothTime = 0.1f;
+	float speedSmoothVelocity;
+	float currentSpeed;
+    float velocityY;
+
+	Animator animator;
+    Transform cameraT;
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start(){
+    	animator = GetComponent<Animator> ();
+        cameraT = Camera.main.transform;
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update(){
+        // Input
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        Move(inputDir, running);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Jump();
+        }
+
+        //Animator
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * 0.5f);
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+    void Move(Vector2 inputDir, bool running){
+        if(inputDir != Vector2.zero){
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+        }
+
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+        grounded = false;
+        velocityY -= Time.deltaTime * gravity;
+
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+        grounded = controller.isGrounded;
+        Debug.Log(grounded);
+
+        if(grounded){
+            velocityY = 0;
+        }
+
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * 0.5f);
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+    void Jump(){
+        if(grounded){
+            float jumpVelocity = Mathf.Sqrt(2*gravity*jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+
+    float GetModifiedSmoothTime(float smoothTime){
+        if(grounded){
+            return smoothTime;
+        }
+        if(airControlPercent == 0){
+            return float.MaxValue;
+        }
+        return smoothTime / airControlPercent;
+    }
+}
